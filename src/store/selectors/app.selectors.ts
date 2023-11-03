24c3534787843ebd3aa9e1010672802f722a5f43
zanzipanziby <zanzipanziby@gmail.com>
2023-11-03 12:51:21 +0300

    fix selectors

--- src/store/selectors/app.selectors.ts
+++ src/store/selectors/app.selectors.ts
@@ -1,6 +1,5 @@
 import { RootStateType } from "../store";
 
 export const selectStatus = (state: RootStateType) => state.app.status;
-export const selectInitialized = (state: RootStateType) =>
-  state.app.initialized;
+export const selectInitialized = (state: RootStateType) => state.app.initialized;
 export const selectError = (state: RootStateType) => state.app.error;
